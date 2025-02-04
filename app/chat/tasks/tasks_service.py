7203156d3e096910658b6f777f34278ab40cf9f7
CA-nakamurasota <nakamura.sota@cloud-ace.jp>
2025-02-04 18:55:34 +0900

    Update tasks_service.py
    デバック用print()の削除

--- app/chat/tasks/tasks_service.py
+++ app/chat/tasks/tasks_service.py
@@ -7,9 +7,7 @@ async def get_tasks(user_id: str) -> list | None:
     try:
         client = firestore_async.client()
         user_data = await client.collection("users").document(user_id).get()
-        print(user_data)
         user = user_data.to_dict()
-        print(user)
         tasks = user.get("tasks")
         if tasks is None:
             raise NoTasksFoundError('No tasks found')
